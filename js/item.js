3e3f8750e66c115e9fe4b39006eb699880aef615
hellookittyyy <hellookittyyy@users.noreply.github.com>
2024-12-08 20:58:08 +0200

    Added drone characteristics to page.

--- js/item.js
+++ js/item.js
@@ -25,6 +25,35 @@ async function loadInfo() {
   document.getElementById("lang-currency").innerHTML = currency;
 
   // characteristics
+  const characteristics = document.getElementById("item-characteristics");
+
+  if (drone.characteristics == null || drone.characteristics.length == 0) {
+    const row = document.createElement("tr");
+    const td1 = document.createElement("td");
+    const td2 = document.createElement("td");
+    td1.id = "lang-noCharacteristics";
+    td2.id = "lang-noCharacteristics";
+    td1.innerHTML = data["noCharacteristics"];
+    td2.innerHTML = data["noCharacteristics"];
+    row.appendChild(td1);
+    row.appendChild(td2);
+    characteristics.appendChild(row);
+  }
+
+  for (let i = 0; i < drone.characteristics.length; i++) {
+    const characteristic = drone.characteristics[i];
+    const row = document.createElement("tr");
+    const td1 = document.createElement("td");
+    const tag = "lang-" + characteristic.title;
+    td1.id = tag;
+    td1.classList.add("characteristic-title");
+    td1.innerHTML = data[characteristic.title];
+    const td2 = document.createElement("td");
+    td2.innerHTML = characteristic.description;
+    row.appendChild(td1);
+    row.appendChild(td2);
+    characteristics.appendChild(row);
+  }
 }
 
 window.addEventListener("load", loadInfo);
